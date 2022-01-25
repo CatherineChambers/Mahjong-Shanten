7f2385490587d85cf1bf371cb000b95cecda64ad
CatherineChambers <katchambers18@outlook.com>
2022-01-25 15:06:07 +0000

    Removed the unnecessary use of tiles_array. Renamed input_hand_to_tiles_array to map_input_hand to reflect this. Added comments to map_input_hand. Added docstrings to class and map_input_hand.

--- Mahjong_Hand_Efficiency.py
+++ Mahjong_Hand_Efficiency.py
@@ -1,29 +1,45 @@
 import numpy as np
+
 import Random_Starting_Hand
 
 
 class MahjongEfficiency(object):
+    """
+    Class to generate the shanten of a given Japanese mahjong hand.
+
+    Attributes:
+        hand : str
+            The 14 tile input hand.
+
+    Methods:
+        error_handling():
+            Checks that the input contains 14 tiles and a maximum of 4 of each tile.
+        map_input_hand():
+            Returns the hand in the mapped format ready for further computation.
+    """
     def __init__(self, hand):
         self.hand = hand
         self.shanten_count = 0
-        tiles = np.linspace(1, 34, 34, dtype=int)
-        self.tiles_array = np.concatenate([tiles, tiles, tiles, tiles])
         self.hand_array = []
 
         # ----------------------------
         self.error_handling()
-        self.input_hand_to_tiles_array()
+        self.map_input_hand()
         self.check_tile_combinations()
         self.count_tile_amount()
 
     def error_handling(self):
         stripped_hand = "".join([char for char in self.hand if char.isdigit()])
-        if len(stripped_hand) != 14:  # If the number of digits is not 14
+        if len(stripped_hand) != 14:
             raise Exception("Please input a valid starting hand of 14 tiles.")
 
-    def input_hand_to_tiles_array(self):
+    def map_input_hand(self):
+        """
+        Take the input hand and maps it to: 1-9 manzu, 10-18 pinzu, 19-27 souzu, 28-34 jihai.
+        Duplicates, red fives, and flowers tiles excluded.
+        """
         index = []
-        mapping_array = [["m", -1], ["p", 9], ["s", 18], ["z", 27]]
+        mapping_array = [["m", 0], ["p", 9], ["s", 18], ["z", 27]]
         mutable_hand = self.hand
         stripped_hand_suits = [char for char in self.hand if not char.isdigit()]
 
@@ -35,11 +51,15 @@ class MahjongEfficiency(object):
                         last_index = 0
                     else:
                         last_index = index
+                    # Finds the position of each of "mpsz" in the input hand.
                     index = mutable_hand.index(mapping_array[i][0])
+                    # Removes the suit character from the hand so that subsequent indexing gives the correct index.
+                    # e.g. 123m2p vs 1232p: the index of p should be 4.
                     mutable_hand = mutable_hand.replace(suit, "", 1)
+                    #  Gets the tiles of each suit, maps them to the new numbers, then appends them to the hand_array.
                     tiles = mutable_hand[last_index:index]
                     tiles = [int(k) for k in tiles]
-                    self.hand_array.append(self.tiles_array[[k + mapping_array[i][1] for k in tiles]])
+                    self.hand_array.append([k + mapping_array[i][1] for k in tiles])
 
     def check_tile_combinations(self):
         pass
