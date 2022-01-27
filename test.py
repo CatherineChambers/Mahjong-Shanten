6ab8f3aba8c6f9d9c6d3bb680993fa51d7db15ed
CatherineChambers <katchambers18@outlook.com>
2022-01-27 17:20:23 +0000

    Added test.py.

---
+++ test.py
@@ -0,0 +1,6 @@
+from src.Mahjong_Hand_Efficiency import MahjongEfficiency
+from src.Random_Starting_Hand import random_starting_hand
+
+if __name__ == "__main__":
+    random_hand = random_starting_hand(1)
+    MahjongEfficiency(random_hand)
