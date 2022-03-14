7d818d8d58c542fd45c82c37dda0780921f8ef01
CatherineChambers <katchambers18@outlook.com>
2022-03-14 14:21:43 +0000

    Removed over-complicated use of itertools and randomly chosen melds. Therefore, removed: tile_combinations, meld_type, couple_type, count_meld_type. Added count_melds, which counts the melds in each suit in order.

--- src/Mahjong_Hand_Efficiency.py
+++ src/Mahjong_Hand_Efficiency.py
@@ -1,6 +1,3 @@
-import itertools
-
-
 class MahjongEfficiency(object):
     """
     Class to generate the shanten of a given Japanese Mahjong hand.
@@ -10,32 +7,17 @@ class MahjongEfficiency(object):
             Checks that the input contains 14 tiles and a maximum of 4 of each tile.
         map_input_hand():
             Generates the hand in the mapped format ready for further computation.
-        tile_combinations():
-            Generates lists containing all the melds, couples and eyes that can be formed from each suit.
-        meld_type():
-            Groups the melds into two types: shuntsu and ankou (see method for details).
-        couple_types():
-            Groups the couples into four types: kanchan, penchan, ryanmen, and pair (see method for details).
+        count_meld():
+            Counts the number of melds (shuntsu and ankou), couples (penchan, ryanmen and kanchan), and pairs
+        get_shanten():
+            Generates the normal shanten, kokushi shanten, and chiitoitsu shanten.
+        output():
+            Prints the final shanten.
     """
 
     def __init__(self, hand):
-        """
-
-        :param hand:
-        """
         # hand = input("Please input a hand: ")
-        self.complete_meld = self.incomplete_meld = 0
         self.hand_array = []
-
-        self.melds = []
-        self.couples = []
-
-        self.shuntsu = []
-        self.ankou = []
-        self.kanchan = []
-        self.penchan = []
-        self.ryanmen = []
-        self.pair = []
 
         self.mapping_array = [["m", 0], ["p", 9], ["s", 18], ["z", 27]]
         self.floor = [1, 10, 19, 28]
@@ -47,10 +29,6 @@ class MahjongEfficiency(object):
         self.map_input_hand()
         self.error_handling()  # This second call checks that the maximum number of each tile is 4.
         print("Hand: {}".format(self.hand))
-
-        combinations = []
-        for i in range(0, len(self.floor)):
-            combinations.append(self.tile_combinations(self.floor[i], self.ceiling[i]))
 
         self.shanten = self.get_shanten(self.floor, self.ceiling)
         self.output()
@@ -96,93 +74,57 @@ class MahjongEfficiency(object):
                     self.hand_array.extend(tiles)
         self.hand_array.sort()
 
-    def tile_combinations(self, floor: int, ceiling: int):
-        """
-        Takes the floor and ceiling of a suit and generates tile combinations of melds, couples, where melds
-        are groups of three and couples are groups of two. Uses these to then generate waits
-        types in self.meld_type() and self.couple_type().
-        """
-        suit = [i for i in self.hand_array if ceiling >= i >= floor]
-        self.melds = list(set([i for i in itertools.combinations(suit, 3)]))
-        self.couples = list(set([i for i in itertools.combinations(suit, 2)]))
-        for i in range(0, len(self.melds)):
-            self.meld_type(self.melds[i])
-        for i in range(0, len(self.couples)):
-            if min(self.couples[i]) >= self.floor[3]:
-                self.couple_type(self.couples[i], self.floor[3], self.ceiling[3])
-            elif self.floor[3] >= min(self.couples[i]) >= self.floor[2]:
-                self.couple_type(self.couples[i], self.floor[2], self.ceiling[2])
-            elif self.floor[2] >= min(self.couples[i]) >= self.floor[1]:
-                self.couple_type(self.couples[i], self.floor[1], self.ceiling[1])
+    def count_melds(self, suit):
+        jihai = [1 for _ in suit if self.floor[3] <= suit[0] and len(suit) > 0]
+        complete_melds = 0
+        incomplete_melds = 0
+        pairs = 0
+        complete_melds_true = 1
+        while len(suit) > 1:
+            if len(suit) > 2 and complete_melds_true:  # Loop for complete melds. They are always of length 3.
+                if suit[0] == (suit[1] == suit[2] or suit[0] == suit[1] - 1 == suit[2] - 2) and not jihai:
+                    complete_melds += 1
+                    del suit[0:3]  # Need to delete the meld from the suit so that numbers are not counted twice.
+                elif suit[0] == suit[1] == suit[2]:  # Jihai can only have ankou.
+                    complete_melds += 1
+                    del suit[0:3]
+                else:  # If there are no more complete melds, we need to stop the loop.
+                    complete_melds_true = 0
             else:
-                self.couple_type(self.couples[i], self.floor[0], self.ceiling[0])
+                if (suit[0] == suit[1] - 1 or suit[0] == suit[1] - 2) and not jihai:
+                    incomplete_melds += 1
+                    del suit[0:2]
+                elif suit[0] == suit[1]:
+                    pairs += 1
+                    del suit[0:2]
+                else:
+                    del suit[0]
+                    # If there are no incomplete melds, we need to remove the first value and continue.
+                    # Because two incomplete melds can be more than 4 tiles away from one another, if we break it will
+                    # not count the second couple.
+        return complete_melds, incomplete_melds, pairs
 
-    def meld_type(self, combination: tuple[int, int, int]):
-        """ Takes melds and groups them into shuntsu or ankou shapes given the following:
-        shuntsu: (n, n+1, n+2) | ankou: (n, n, n)\n where n is in range(1, 34)."""
-        if min(combination) >= self.floor[3]:  # Jihai can only be ankou (or pair)
-            if combination[0] == sum(combination) / len(combination):
-                self.ankou.append(combination)
-        else:
-            if combination[1] == sum(combination) / len(combination) and combination[1] == combination[0] + 1:
-                self.shuntsu.append(combination)
-            if combination[0] == sum(combination) / len(combination):
-                self.ankou.append(combination)
-
-    def couple_type(self, combination: tuple[int, int], floor: int, ceiling: int):
-        """ Takes couples and groups them into kanchan, penchan or ryanmen waits, or pairs, given the following:
-        | kanchan: (n, n+2) | penchan: (1, 2) or (8, 9) | ryanmen: (n, n+1) (excluding penchan waits) | pair: (n, n)
-        \n where n is in range(1, 34)."""
-        if floor >= self.floor[3]:  # Jihai can only be pair (or ankou)
-            if combination[0] == combination[1]:
-                self.pair.append(combination)
-        else:
-            if (combination[1] == combination[0] + 1) and (combination[0] > floor and combination[1] < ceiling):
-                self.ryanmen.append(combination)
-            if (combination[1] == combination[0] + 1) and (combination[0] == floor or combination[1] == ceiling):
-                self.penchan.append(combination)
-            if combination[1] == combination[0] + 2:
-                self.kanchan.append(combination)
-            if combination[0] == combination[1]:
-                self.pair.append(combination)
-
-    # Still does not correctly work for all hands, e.g. "11122233456789m". Gives 0 when it is a ready hand.
-    # And for "14579m1226p233s66z" it gives 2 when it should be 3
-    # Can't use all combinations of melds because it takes too long, as expected.
-    # Perhaps I need to get the complete melds first, then the incomplete melds from the rest of the hand (excluding
-    # complete melds.
     def get_shanten(self, floor: list, ceiling: list) -> (int, int, int):
+        complete_meld = 0
+        incomplete_meld = 0
+        pair = 0
+        for i in range(0, 4):
+            suit = [tile for tile in self.hand_array if floor[i] <= tile <= ceiling[i]]
+            meld, couple, pairs = self.count_melds(suit)
+            complete_meld += meld
+            incomplete_meld += couple
+            pair += pairs
+
         terminals = {i for i in self.hand_array if i in floor or i in ceiling or i > 27}
-        if [i for pair in self.pair for i in pair if i == (floor or ceiling)]:
+        terminals_full = [i for i in self.hand_array if i in floor or i in ceiling or i > 27]
+        if len(terminals) != len(terminals_full):  # If there is a pair
             kokushi_shanten = 13 - len(terminals) - 1
         else:
             kokushi_shanten = 13 - len(terminals)
-        chiitoitsu_shanten = 6 - len(self.pair)
-        shuntsu = list(itertools.permutations(self.shuntsu))
-        shuntsu = [shuntsu[k] for k in range(0, len(shuntsu))]
-        shanten = []
-        meld_tiles = self.penchan + self.kanchan + self.ryanmen + self.pair + shuntsu + self.ankou
-        melds = self.count_meld_type(meld_tiles)
-        self.complete_meld = 0
-        self.incomplete_meld = 0
-        for group in melds:
-            if len(group) == 3:
-                self.complete_meld += 1
-            else:
-                self.incomplete_meld += 1
-        shanten.append(8 - 2*self.complete_meld - self.incomplete_meld)
-        shanten = min(shanten)
-        return chiitoitsu_shanten, shanten, kokushi_shanten
+        chiitoitsu_shanten = 6 - pair
 
-    def count_meld_type(self, melds):
-        tiles = [i for j in range(0, len(melds)) for i in melds[j]]
-        for tile in tiles:
-            dup_index = [x for x, y in enumerate(melds) if tile in y]
-            if dup_index:
-                for k in range(0, len(dup_index) - 1):
-                    del melds[dup_index[k]]
-                    break
-        return melds
+        shanten = 8 - 2 * complete_meld - incomplete_meld - pair
+        return chiitoitsu_shanten, shanten, kokushi_shanten
 
     def output(self):
         shanten = min(self.shanten)
