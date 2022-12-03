381de131441055fb135226a9fd535a979b5b6678
CatherineChambers <katchambers18@outlook.com>
2022-12-03 13:39:55 +0000

    Changed Random_Starting_Hand.py to properly output multiple hands when the input is greater than one. test.py changed to reflect the new list output.

--- src/Random_Starting_Hand.py
+++ src/Random_Starting_Hand.py
@@ -1,24 +1,29 @@
 import numpy as np
 
 
-def random_starting_hand(input_number: int) -> str:
+def random_starting_hand(input_number: int) -> list:
     """ Generates a number of random Mahjong hands specified by the input integer. """
     k = 0
-    while True:
-        if k == int(input_number):
-            break
+    final_output = []
+    while k < int(input_number):
         k += 1
+
+        # Creates tiles_by_code, which lists the number of tiles 1-9 of each suit (1-7 for jihai)
         suits = np.linspace(1, 9, 9, dtype=int)
         tiles_by_code = np.concatenate([suits, suits, suits, np.linspace(1, 7, 7, dtype=int)])
+        # Creates a tiles_array, which lists the number of non-duplicated tiles in each suit 1-34
         tiles = np.linspace(1, 34, 34, dtype=int)
         tiles_array = np.concatenate([tiles, tiles, tiles, tiles])
 
         chosen_tiles = np.random.choice(tiles_array, 14, replace=False)
 
+        # Attributes the tiles to the correct suit and creates the hand using standard tile notation
         manzu = sorted([tiles_by_code[i - 1] for i in chosen_tiles if i < 10])
         pinzu = sorted([tiles_by_code[i - 1] for i in chosen_tiles if 9 < i < 19])
         souzu = sorted([tiles_by_code[i - 1] for i in chosen_tiles if 18 < i < 28])
         jihai = sorted([tiles_by_code[i - 1] for i in chosen_tiles if 27 < i < 35])
         hand_as_list = manzu + list("m") + pinzu + list("p") + souzu + list("s") + jihai + list("z")
         hand_output = "".join(str(i) for i in hand_as_list)
-        return hand_output
+
+        final_output.append(hand_output)
+    return final_output
--- test.py
+++ test.py
@@ -2,5 +2,5 @@ from src.Mahjong_Hand_Efficiency import MahjongEfficiency
 from src.Random_Starting_Hand import random_starting_hand
 
 if __name__ == "__main__":
-    random_hand = random_starting_hand(1)
+    random_hand = random_starting_hand(1)[0]
     MahjongEfficiency(random_hand)
